910aab749dcc9848f4d2f875fe67c85e38837e76
JODAHYE <dahye8043@gmail.com>
2020-05-24 01:05:05 +0900

    dd

--- allpictures/views.py
+++ allpictures/views.py
@@ -13,6 +13,6 @@ from home.models import Post
 def picture_list(request):
     page = request.GET.get('page', 1)
     object_list = Post.objects.all().order_by('-created')
-    paginator = Paginator(object_list, 16)
+    paginator = Paginator(object_list, 24)
     objects = paginator.get_page(page)
     return render(request, 'allpictures/picture_list.html', context={"objects": objects})
